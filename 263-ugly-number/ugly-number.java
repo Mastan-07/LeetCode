78965997eefe3b87fc540d98aaf3e10a4bc6013d
Mastanvali Shaik <113561965+Umarmastan@users.noreply.github.com>
2025-02-08 20:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (43.70%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n <= 0) return false;
-        int[] factors = {2, 3, 5};
-        
-        for (int factor : factors) {
-            while (n % factor == 0) {
-                n /= factor;
-            }
+        if(n<=0)
+        {
+            return false;
         }
-        
+        while(n%2 == 0)
+        {
+            n=n/2;
+        }
+        while(n%3 == 0)
+        {
+            n=n/3;
+        }
+        while(n%5 == 0)
+        {
+            n=n/5;
+        }
         return n == 1;
+        
     }
 }
