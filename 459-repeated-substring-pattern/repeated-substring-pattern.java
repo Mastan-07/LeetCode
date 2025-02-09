6d3419d3844edaa4f4727634e48f4571a76f668e
Mastanvali Shaik <113561965+Umarmastan@users.noreply.github.com>
2025-02-09 09:26:50 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (97.23%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,7 +1,23 @@
-public class Solution {
+class Solution {
+    private static boolean check(String s, String substr) {
+        return s.endsWith(substr) && substr.repeat(s.length() / substr.length()).equals(s);
+    }
     public boolean repeatedSubstringPattern(String s) {
-        String doubled = s + s;
-        String sub = doubled.substring(1, doubled.length() - 1);
-        return sub.contains(s);
+        int slen = s.length();
+
+        int upTo = (int) Math.ceil(Math.sqrt(slen));
+        for (int i = 1; i <= upTo; ++i) {
+            if (slen % i == 0) {
+                int len1 = i;
+                if (len1 != slen && check(s, s.substring(0, len1))) {
+                    return true;
+                }
+                int len2 = slen / i;
+                if (len2 != 1 && len2 != slen && check(s, s.substring(0, len2))) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
